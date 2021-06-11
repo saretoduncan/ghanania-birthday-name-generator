cf352ef10d2b5cf8621cc47d9426691fff296cd0
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-11 14:53:55 +0300

    add days of the week and days of the month per year arrays

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,13 @@
+//male names according to the day of the week
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
+
+//female names according to the day of the week
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //Days of the week
+const totalDaysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] //total Days Of Months in a year
+  // var monthCode = [] //month code according to Gregian calendar
+  //date of birth data collection function
+
 var dataCollection = (day, month, year) => {
   let dd = day;
   let mm = month;
