10f59cb3ad073f60243c2191a4c41404cbe50445
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-11 17:52:14 +0300

    add century code generator

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,15 @@
 //male names according to the day of the week
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
-
+//...//
 //female names according to the day of the week
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+//...//
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //Days of the week
 var noOfDays = 7 //number of days in a week
-const totalDaysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] //total Days Of Months in a year
-var monthCode = [] //month code according to Gregian calendar
+  //total Days Of Months in a year
+const totalDaysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  //...//
+var monthCode = [] //month code according to Gregian calendar system
 var monthCodeGenerator = () => {
     let mod = 0;
     for (let i = 0; i < totalDaysOfMonths.length; i++) {
@@ -17,9 +20,21 @@ var monthCodeGenerator = () => {
     return mod;
   } //month Code Generator
 monthCodeGenerator();
+var centuryCode = century => {
+    let code;
+    if (century % 4 === 0) {
+      code = 6;
+    } else if (century === 1 || (century - 1) % 4 === 0) {
+      code = 4;
+    } else if ((century + 1) % 4 === 0) {
+      code = 0;
+    } else if (century === 2 || (century / 2) % 2 !== 0) {
+      code = 2;
+    }
+    return code;
+  } // century code according to Gregian calendar system
 
 //date of birth data collection function
-
 var dataCollection = (day, month, year) => {
   let dd = day;
   let mm = month;
