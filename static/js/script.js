18cd8cd2748440bfcfb9a49c1fc784bf420a7d13
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-11 13:53:58 +0300

    add arrays of male and female name

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var dataCollection = (day, month, year) => {
   let dd = day;
   let mm = month;
   let cc = Math.floor(year / 100);
   let yy = parseInt(year.slice(-2));
-  console.log(`${dd}, ${mm}, ${cc}, ${yy}`)
 }
