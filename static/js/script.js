9c284d5baa27d1cff5aa400725d825cc17fd42d8
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-11 15:11:18 +0300

    add month code generator function that generate code according to gregian calendar system

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,21 @@ const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame
 //female names according to the day of the week
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //Days of the week
+var noOfDays = 7 //number of days in a week
 const totalDaysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] //total Days Of Months in a year
-  // var monthCode = [] //month code according to Gregian calendar
-  //date of birth data collection function
+var monthCode = [] //month code according to Gregian calendar
+var monthCodeGenerator = () => {
+    let mod = 0;
+    for (let i = 0; i < totalDaysOfMonths.length; i++) {
+      monthCode.push(mod);
+      mod = (mod + totalDaysOfMonths[i]) % noOfDays
+
+    }
+    return mod;
+  } //month Code Generator
+monthCodeGenerator();
+
+//date of birth data collection function
 
 var dataCollection = (day, month, year) => {
   let dd = day;
